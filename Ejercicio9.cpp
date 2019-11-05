d6684c0dfae28af705a64fb47ae59646fe921a24
Marco Fiorito <marcofiorito1@hotmail.com>
2019-11-04 23:17:38 -0300

    working for case bases and debug

--- Ejercicio9.cpp
+++ Ejercicio9.cpp
@@ -43,23 +43,46 @@ nodeResult *add(nodeResult *currentNode, pii newNode)
     if (currentNode->quantityOfElements > 0 && currentNode->result[currentNode->quantityOfElements - 1].first == newNode.first)
     {
         currentNode->result[currentNode->quantityOfElements - 1].second = currentNode->result[currentNode->quantityOfElements - 1].second > newNode.second ? currentNode->result[currentNode->quantityOfElements - 1].second : newNode.second;
-
         return currentNode;
     }
     currentNode->result[currentNode->quantityOfElements] = newNode;
     currentNode->quantityOfElements++;
+    return currentNode;
 }
 
-nodeResult *merge(nodeResult *left, nodeResult *right, int lenghtLeft, int lengthRight)
+void print(nodeResult *p)
+{
+    cout << "exec" << endl;
+    for (int i = 0; i < p->quantityOfElements; i++)
+    {
+        cout << "(" << p->result[i].first << "," << p->result[i].second << ")" << endl;
+    }
+}
+
+nodeResult *merge(nodeResult *left, nodeResult *right)
 {
     nodeResult *result = new nodeResult(left->quantityOfElements + right->quantityOfElements);
     int height1 = 0, height2 = 0;
-
+    // print(left);
+    // print(right);
     int i = 0;
     int j = 0;
     while (i < left->quantityOfElements && j < right->quantityOfElements)
     {
-        if (left->result[i].first < right->result[j].first)
+        // cout << "i es: " << i << " j es: " << j << endl;
+        if (left->result[i].first == right->result[j].first)
+        {
+            int position = left->result[i].first;
+            height1 = left->result[i].second;
+            height2 = right->result[j].second;
+            pii newNode;
+            newNode.first = position;
+            newNode.second = height1;
+            result = add(result, newNode);
+            j++;
+            i++;
+        }
+        else if (left->result[i].first < right->result[j].first)
         {
 
             int leftPosition = left->result[i].first;
@@ -81,15 +104,22 @@ nodeResult *merge(nodeResult *left, nodeResult *right, int lenghtLeft, int lengt
             j++;
         }
     }
+
     while (i < left->quantityOfElements)
     {
+        // cout << "i es: " << i << endl;
+        // cout << right->result[i].first << "," << right->result[i].second << endl;
         result = add(result, left->result[i]);
-
         i++;
     }
     while (j < right->quantityOfElements)
     {
+        // cout << " j es: " << j << endl;
+        // cout << right->result[j].first << "," << right->result[j].second << endl;
         result = add(result, right->result[j]);
+        // cout << "imprimiendo result" << endl;
+        // print(result);
+        // cout << "terminde de imprimir result" << endl;
         j++;
     }
     return result;
@@ -102,20 +132,34 @@ nodeResult *getContours(node **shades, int left, int right)
     if (left == right)
     {
         nodeResult *result = new nodeResult(2);
-        result->result[0].first = shades[left]->initialCoordinate;
-        result->result[0].second = shades[left]->height;
-        result->result[1].first = shades[left]->finalCoordinate;
-        result->result[1].second = 0;
-        result->quantityOfElements++;
-        result->quantityOfElements++;
+
+        // result->result[0].first = shades[left]->initialCoordinate;
+        // result->result[0].second = shades[left]->height;
+
+        pii newNode1;
+        newNode1.first = shades[left]->initialCoordinate;
+        newNode1.second = shades[left]->height;
+        result = add(result, newNode1);
+
+        // result->result[1].first = shades[left]->finalCoordinate;
+        // result->result[1].second = 0;
+
+        pii newNode2;
+        newNode2.first = shades[left]->finalCoordinate;
+        newNode2.second = 0;
+        result = add(result, newNode2);
+
+        // result->quantityOfElements++;
+        // result->quantityOfElements++;
         return result;
     }
     else
     {
+
         int middle = (right + left) / 2;
         l = getContours(shades, left, middle);
         r = getContours(shades, middle + 1, right);
-        return merge(l, r, middle - left + 1, right - middle);
+        return merge(l, r);
     }
 }
 
